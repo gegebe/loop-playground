4db70be31b8b941d7dddf019962662bcf6ad1a74
gegebe <ggirbes@gmail.com>
2023-10-10 12:31:24 +0200

    while

--- ex8.js
+++ ex8.js
@@ -4,13 +4,15 @@
  * Modifica el bucle while para que escriba por el terminal 
  * escriba 5 veces exactamente "Ejecuto!". Actualmente lo escribe 6 veces. Porque?
  * 
+ * >>Porque la comparación estaba en < o igual, hay que pasarla a menor a veces, porque de 0 a 5 son 6 posiciones.
+ * 
  * Reestricción: NO puedes cambiar el valor de las variables 'veces' ni 'i' !!
  */
 
 let veces = 5
 let i = 0
 
-while (i<=veces) {
+while (i<veces) {
     console.log("Ejecuto!")
     i++
 }
