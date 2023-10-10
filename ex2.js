b679b44c8e6cc94eec944f0e74b69a467a52bc7b
gegebe <ggirbes@gmail.com>
2023-10-10 11:47:13 +0200

    for

--- ex2.js
+++ ex2.js
@@ -3,6 +3,13 @@
  * que muestre por el terminal el mensaje "Yo también estoy en un bucle!" ; un total de 20 veces
  */
 
+let bucle;
+bucle = 20;
+
+for(i = 0; i < bucle; i++){
+  console.log("Yo también estoy en un bucle!");
+}
+
 /**
  * Resultado esperado: https://oscarm.tinytake.com/msc/ODc4MzU2M18yMjIwMzc5Ng
  */
