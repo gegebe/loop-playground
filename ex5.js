cb34d67352fb364b216616648e14c712b7d1976f
gegebe <ggirbes@gmail.com>
2023-10-10 11:54:19 +0200

    corregir parametro

--- ex5.js
+++ ex5.js
@@ -3,12 +3,14 @@
  * Comrpruébalo con "node ex5.js"
  * ¿Sabrías decir porque?
  *
+ * >> El valor de i es mayor a la comparación, por tanto nunca entra en el bucle y no llega a la acción de incrementar
+ * 
  * ---------------------------------------------------
  * Una vez lo averigues, arreglalo para que el bucle se ejecute exactamente 10 veces.
  * ATENCIÓN: SOLO puedes cambiar el valor de la variable 'veces' para conseguirlo
  */
 
-let veces = 5;
+let veces = 15;// de 5 a 15
 
 // PROHIBIDO modificar el código a partir de aquí
 
