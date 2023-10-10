3add0d19e57b7deb8f285e16a934bd8fbfeb982b
gegebe <ggirbes@gmail.com>
2023-10-10 11:42:27 +0200

    switch

--- ex0.js
+++ ex0.js
@@ -11,4 +11,70 @@
 
 let mes = 4;
 
-// Escribir tu código a partir de aquí
+//Escribir tu código a partir de aquí
+
+//let Enero = 1;
+
+// let
+// enero = 1,
+// febrero = 1,
+// marzo = 1;
+
+// let
+// abril = 2,
+// mayo = 2,
+// junio = 2;
+
+// let
+// julio = 3,
+// agosto = 3,
+// septiembre = 3;
+
+// let
+// octubre = 4,
+// noviembre = 4,
+// diciembre = 4;
+
+// let mesUser = "Enero";
+
+// switch(enero){
+//   case 1:
+//     console.log(`El mes de ${mesUser} es del primer trimestre`);
+//     break;
+//   case 2:
+//     console.log(`El mes de ${mesUser} es del primer trimestre`);
+//     break;
+//   case 3:
+//     console.log(`El mes de ${mesUser} es del primer trimestre`);
+//     break;
+//   case 4:
+//     console.log(`El mes de ${mesUser} es del primer trimestre`);
+//     break;
+//   default:
+//   break;
+// };
+
+switch(mes){
+  case 1:
+  case 2:
+  case 3:
+    console.log("Primer trimestre");
+    break;
+  case 4:
+  case 5:
+  case 6:
+    console.log("Segundo trimestre");
+    break;
+  case 7:
+  case 8:
+  case 9:
+    console.log("Tercer trimestre");
+  case 10:
+  case 11:
+  case 12:
+    console.log("Cuarto trimestre");
+    break;
+  default:
+    console.log("El mes no corresponde con ningún trimestre");
+    break;
+}
